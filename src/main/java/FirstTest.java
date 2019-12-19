b983843f2b93a9cea187d568a61ba59d171682ad
Tal Lavi <tal@eyenet-mobile.com>
2019-12-19 14:42:59 +0200

    better messages

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -3,15 +3,15 @@ import org.junit.jupiter.api.Test;
 class FirstTest  {
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("myMainA START");
-        Thread.sleep(5000);
-        System.out.println("myMain END");
+        System.out.println("main START");
+//        Thread.sleep(5000);
+        System.out.println("main END");
     }
 
     @Test
     void myTest() throws InterruptedException {
         System.out.println("test START");
-        Thread.sleep(5000);
+//         Thread.sleep(5000);
         System.out.println("test END");
     }
 }
